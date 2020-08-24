22ce916a3fa1fc2b549f763b9d81202644215016
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-24 03:32:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
- name_hash.collect do|key,value|
+ smallest_value = 0
  empty_hash = nil
-  if value <=1 
-   key 
+ name_hash.each do |name, value|
+  if smallest_value == 0 || value < smallest_value
+    smallest_value = value
+    empty_hash = name
   end
-     empty_hash
- end 
+ end
+ empty_hash
 end
